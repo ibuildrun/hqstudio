d4245a6f8889b0301dd90d0056f0eb0b6883eaf3
randomu3 <demiz99@mail.ru>
2025-12-22 17:20:13 +0500

    feat(desktop): добавлен конвертер видимости для пагинации

---
+++ HQStudio.Desktop/Converters/GreaterThanOneToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HQStudio.Converters
+{
+    public class GreaterThanOneToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue)
+                return intValue > 1 ? Visibility.Visible : Visibility.Collapsed;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
