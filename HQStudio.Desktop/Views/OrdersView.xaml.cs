c8bbd367ef4d984a87f6ecb6909435065b394890
randomu3 <demiz99@mail.ru>
2025-12-23 22:31:18 +0500

    fix(desktop): исправлен индикатор выбранного заказа через code-behind

--- HQStudio.Desktop/Views/OrdersView.xaml.cs
+++ HQStudio.Desktop/Views/OrdersView.xaml.cs
@@ -3,11 +3,14 @@ using HQStudio.ViewModels;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace HQStudio.Views
 {
     public partial class OrdersView : UserControl
     {
+        private Border? _lastSelectedIndicator;
+        
         public OrdersView()
         {
             InitializeComponent();
@@ -15,11 +18,29 @@ namespace HQStudio.Views
 
         private void Order_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (sender is FrameworkElement element && element.DataContext is Order order)
+            if (sender is Border cardBorder && cardBorder.DataContext is Order order)
             {
                 if (DataContext is OrdersViewModel vm)
                 {
-                    // Одиночный клик - выбор заказа
+                    // Сбрасываем предыдущее выделение
+                    if (_lastSelectedIndicator != null)
+                    {
+                        _lastSelectedIndicator.Background = Brushes.Transparent;
+                    }
+                    
+                    // Находим индикатор в текущей карточке
+                    var grid = cardBorder.Child as Grid;
+                    if (grid != null)
+                    {
+                        var indicator = grid.Children.OfType<Border>().FirstOrDefault(b => b.Name == "SelectionIndicator");
+                        if (indicator != null)
+                        {
+                            indicator.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#4CAF50"));
+                            _lastSelectedIndicator = indicator;
+                        }
+                    }
+                    
+                    // Устанавливаем выбранный заказ
                     vm.SelectedOrder = order;
                     
                     // Двойной клик - редактирование
