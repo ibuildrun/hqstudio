f629b2f7bdb59407d2fd02159f943c36eda18876
randomu3 <demiz99@mail.ru>
2025-12-26 19:00:44 +0500

    chore(desktop): добавлены конвертеры и обновлены зависимости проекта

---
+++ HQStudio.Desktop/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HQStudio.Converters
+{
+    /// <summary>
+    /// Конвертер строки в Visibility (Visible если строка не пустая)
+    /// </summary>
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return string.IsNullOrWhiteSpace(str) ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
